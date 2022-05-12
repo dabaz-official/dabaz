99ba4180f96d63b45c2c1d22bd6782b849555680
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-05-13 02:25:51 +0800

    Change font size

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,22 +4,13 @@ import s from '../styles/skills.module.css'
 const Skills: React.FC = () => {
   return (
     <div className="bg-black text-white">
-      <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-4xl md:text-6xl lg:text-7xl tracking-tight font-semibold">
+      <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-2xl md:text-3xl lg:text-4xl tracking-tight font-semibold">
         <div className="leading-[1.15]">
           <div>
             I have more than several skills.
           </div>
           <span>
-            I can help you to find vulnerabilities of your website. 
-          </span>
-          <span>
-            I&apos;m good at music so I can also make beats for your music.
-          </span>
-          <span>
-            I also provide UI design and cover design sevices.
-          </span>
-          <span>
-            I also do some front-end developing.
+            I can help you to find vulnerabilities of your website. I&apos;m good at music so I can also make beats for your music. I also provide UI design and cover design sevices. I also do some front-end developing.
           </span>
         </div>
       </div>
