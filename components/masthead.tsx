464589c2f0d739d8d3b462ae69570cc37a4fb541
DabAZ <76861837+dabaz-official@users.noreply.github.com>
2022-05-13 02:13:09 +0800

    Update masthead.tsx

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -8,9 +8,7 @@ const Masthead: React.FC = () => {
         <source src="/assets/bg.mp4" type="video/mp4; codecs=hvc1;" />
         <source src="/assets/bg.webm" type="video/webm; codecs=vp9" />
       </video>
-      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000 ${
-        imageLoaded ? 'opacity-100' : 'opacity-0'
-      }`}>
+      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
         <Image src="/assets/logo.svg" width={128 / 3} height={128 / 3} alt="DabAZ Logo" />
       </div>
       <div className="p-12 font-bold z-10 text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)] text-center flex-1 flex items-center justify-center flex-col">
